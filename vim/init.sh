9075cd15f29356e9290a4b520e3fc40df6232c80
underson666666 <underson666666@@gmail.com>
2020-10-25 20:11:29 +0900

    init.shを復元

--- vim/init.sh
+++ vim/init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ln -s $HOME/.vimrc .vimrc
 ln -s $HOME/.cheatsheet .cheatsheet
 ln -s $HOME/.vim .vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
