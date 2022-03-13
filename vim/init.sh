63a08bb29a0d818cdb113335ed247e05f2f7b903
underson666666 <underson666666@@gmail.com>
2022-03-13 23:04:18 +0900

    vimrc用のinit.shを修正

--- vim/init.sh
+++ vim/init.sh
@@ -1,7 +1,23 @@
 #!/bin/sh
-ln -s `pwd`/.vimrc ~/.vimrc
-ln -s `pwd`/.cheatsheet ~/.cheatsheet
-ln -s `pwd`/.vim ~/.vim
+
+ORGDIR=orig_vim_settings/
+mkdir $ORGDIR
+if [ -f ~/.vimrc ] ; then
+    mv ~/.vimrc $ORGDIR
+fi
+if [ -d ~/.vim ] ; then
+    mv ~/.vim $ORGDIR
+fi
+if [ -d ~/.cheatsheet.md ] ; then
+    mv ~/.cheatsheet.md $ORGDIR
+fi
+
+ln -s `pwd`/.vim $HOME/.vim
+ln -s `pwd`/.vimrc $HOME/.vimrc
+ln -s `pwd`/.cheatsheet.md $HOME/.cheatsheet.md
+
+# install vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# install plugins
 vim -c PlugInstall -c qa
