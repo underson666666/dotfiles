ce0c25bcfd472c51210f92e0978c785f5ce62071
underson666666 <underson666666@@gmail.com>
2022-03-14 21:35:49 +0900

    vim初期化スクリプト変更と不要ディレクトリを削除

--- vim/init.sh
+++ vim/init.sh
@@ -1,23 +1,34 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-ORGDIR=orig_vim_settings/
-mkdir $ORGDIR
-if [ -f ~/.vimrc ] ; then
-    mv ~/.vimrc $ORGDIR
-fi
-if [ -d ~/.vim ] ; then
-    mv ~/.vim $ORGDIR
-fi
-if [ -d ~/.cheatsheet.md ] ; then
-    mv ~/.cheatsheet.md $ORGDIR
+ORGDIR=$HOME/orig_vim_settings/
+if [ ! -d $ORGDIR ]; then
+    mkdir $ORGDIR
+    targets=("$HOME/.vimrc" "$HOME/.vim/userautoload" "$HOME/.cheatsheet.md")
+    for target in "${targets[@]}"
+    do
+        if [ -f $target ] || [ -d $target ] ; then
+            if [ -L $target ] ; then
+                unlink $target
+            else
+                mv $target $ORGDIR
+            fi
+        fi
+    done
 fi
 
-ln -s `pwd`/.vim $HOME/.vim
+if [ ! -d $HOME/.vim ] ; then
+    mkdir $HOME/.vim
+fi
+
 ln -s `pwd`/.vimrc $HOME/.vimrc
+ln -s `pwd`/.vim/userautoload $HOME/.vim/userautoload
 ln -s `pwd`/.cheatsheet.md $HOME/.cheatsheet.md
 
 # install vim-plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+if [ ! -d $HOME/.vim/autoload ]; then
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
 # install plugins
 vim -c PlugInstall -c qa
