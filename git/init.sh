058c0f4ef20a502fb555aa157afd8a919e092422
underson666666 <underson666666@gmail.com>
2022-06-01 18:01:11 +0900

    gitでconflict発生時にvimdiffを使う設定を追加

--- git/init.sh
+++ git/init.sh
@@ -4,3 +4,6 @@ git config --global color.diff auto
 git config --global color.status auto
 git config --global diff.tool vimdiff
 git config --global difftool.prompt false
+git config --global merge.tool vimdiff
+git config --global merge.conflictstyle diff3
+git config --global mergetool.prompt false
