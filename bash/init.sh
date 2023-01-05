7c933b90e98f638aa4f864bfb8cbb4213c3c7d33
underson666666 <underson666666@gmail.com>
2023-01-05 21:25:39 +0900

    bashrc, bash_profieの追加設定の読み込み方法を変更

--- bash/init.sh
+++ bash/init.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-cat .bash_profile_additional >> ~/.bash_profile
-cat .bashrc_additional >> ~/.bashrc
+DIR="$( cd "$( dirname "$0" )" && pwd -P )"
+echo "source $DIR/.bash_profile_additional" >> ~/.bash_profile
+echo "source $DIR/.bashrc_additional" >> ~/.bashrc
 
 source ~/.bash_profile
