903f04fb3e064840bf33c8e18496bfa8b69722c4
underson666666 <underson666666@gmail.com>
2023-02-02 22:00:11 +0900

    bashの追加設定箇所をわかりやすくした

--- bash/init.sh
+++ bash/init.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "$0" )" && pwd -P )"
+
+
+echo "" >> ~/.bash_profile
+echo "#######################" >> ~/.bash_profile
+echo "# additional settings #" >> ~/.bash_profile
+echo "#######################" >> ~/.bash_profile
 echo "source $DIR/.bash_profile_additional" >> ~/.bash_profile
+
+echo "" >> ~/.bashrc
+echo "#######################" >> ~/.bashrc
+echo "# additional settings #" >> ~/.bashrc
+echo "#######################" >> ~/.bashrc
 echo "source $DIR/.bashrc_additional" >> ~/.bashrc
 
 source ~/.bash_profile
