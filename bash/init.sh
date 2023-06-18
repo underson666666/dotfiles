582ece2dfed3289ec206b03587768730bc5b8c51
underson666666 <underson666666@gmail.com>
2023-06-18 13:31:27 +0900

    modify bash init script

--- bash/init.sh
+++ bash/init.sh
@@ -7,11 +7,13 @@ echo "#######################" >> ~/.bash_profile
 echo "# additional settings #" >> ~/.bash_profile
 echo "#######################" >> ~/.bash_profile
 echo "source $DIR/.bash_profile_additional" >> ~/.bash_profile
+echo "" >> ~/.bash_profile
 
 echo "" >> ~/.bashrc
 echo "#######################" >> ~/.bashrc
 echo "# additional settings #" >> ~/.bashrc
 echo "#######################" >> ~/.bashrc
 echo "source $DIR/.bashrc_additional" >> ~/.bashrc
+echo "" >> ~/.bashrc
 
 source ~/.bash_profile
